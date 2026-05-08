8709e93d2fb17080453d7beb0206a6f5cd8b9c05
johannayara <johanna.jacomenoianuding@epfl.ch>
2026-05-08 14:01:54 +0200

    fix: authors_genders

--- data_prep.py
+++ data_prep.py
@@ -4,9 +4,9 @@ import gender_guesser.detector as gender
 """
 Code to generate 3 databases of authors: gendered with ratings, only genders and undefined gender
 Method:
-- Remove titles that are in international bestsellers database 
-- Use gender guesser to get gender of books in best books database 
-- Use international bestseller to add the gender of known books 
+- Remove titles that are in international bestsellers database
+- Use gender guesser to get gender of books in best books database
+- Use international bestseller to add the gender of known books
 """
 
 
@@ -30,15 +30,15 @@ d = gender.Detector()
 def guess_gender(authors: str):
     split_authors = authors.split(",")
 
-    if (len(split_authors) == 1): 
+    if (len(split_authors) == 1):
         name = split_authors[0].split()[0]
         return 'w' if d.get_gender(name) == "female" else 'm'
 
-    # more than one name 
+    # more than one name
     final_gender = ""
     for author in split_authors:
         name = author.split()[0]
-        if len(final_gender) > 0: 
+        if len(final_gender) > 0:
             final_gender += ";"
         gender = d.get_gender(name)
         match gender:
@@ -49,15 +49,13 @@ def guess_gender(authors: str):
             case _:
                 final_gender += gender
     return final_gender
-        
-        
+
+
 
 
 
 
 df['gender']= df["author"].apply(lambda x: guess_gender(x))
-ab = df[['author', 'gender']].copy()
-ab.to_csv("authors_gen.csv", index=False)
 
 # Output unclassified genders
 df_undefined = df[~df['gender'].isin(["m", "w", "m;m", "w;m", "m;w", "w;w"])]
@@ -74,7 +72,7 @@ df_merged = df_merged.drop_duplicates(subset=["title"])
 df_merged.to_csv("genders_ratings.csv", index=False)
 print(f"gender with ratings: {len(df_merged)}")
 
-international_bestsellers = international_bestsellers[["title","author", "gender"]] 
+international_bestsellers = international_bestsellers[["title","author", "gender"]]
 df_merged_gender = pd.concat([df_merged[["title","author", "gender"]], international_bestsellers])
 df_merged_gender = df_merged_gender.drop_duplicates(subset=["title"])
 df_merged_gender.to_csv("genders.csv", index=False)
